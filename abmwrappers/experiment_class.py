d0024422b74a989c2f06362a53b432f6f2b55733
Will Koval <ad71@cdc.gov>
2025-05-21 22:40:21 +0000

    collect distance lazyframe

--- abmwrappers/experiment_class.py
+++ abmwrappers/experiment_class.py
@@ -495,7 +495,8 @@ class Experiment:
         """
         Read distances and simulation results into the simulation bundle history
         """
-        distances = pl.scan_parquet(f"{input_dir}/distances/")
+        # Scan the hive partition parquet file and collect into a dataframe
+        distances = pl.scan_parquet(f"{input_dir}/distances/").collect()
 
         if distances.is_empty():
             raise ValueError("No distances found in the input directory.")
